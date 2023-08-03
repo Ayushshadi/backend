a96591d8d8ec292521b8128c75a3882887077a7f
Ayushshadi <51690555+Ayushshadi@users.noreply.github.com>
2023-08-03 16:49:30 +0530

    Add files via upload

--- product.js
+++ product.js
@@ -1,11 +1,27 @@
-const path=require('path');
-exports.getaddcontroller=(req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../add-product.html'));
-    };
-exports.postAddcontroller=((req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-    });
-exports.showproducts=((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'../','shop.html'));
-});
+
+const db=require('../util/database');
+
+module.exports = class Product {
+  constructor(id,title, imageUrl, description, price) {
+    this.id=id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+   return db.execute('insert into products(title,price,description,imageURL) values(?,?,?,?)',
+            [this.title,this.price,this.description,this.imageUrl]);
+  }
+
+  static deletebyId(id){
+   
+  }
+  static fetchAll() {
+    return db.execute('select * from products');
+  }
+  static findbyId(id){
+return db.execute('select * from products where id=?',[id]);
+  }
+};
